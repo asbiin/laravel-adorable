066b6b5ed7ca9f3d11848799e570107096991194
Alexis Saettler <alexis.saettler@wolterskluwer.com>
2022-01-11 18:00:25 +0100

    test: save real image files (#6)

--- tests/Unit/Adorable/LaravelAdorableTest.php
+++ tests/Unit/Adorable/LaravelAdorableTest.php
@@ -16,10 +16,11 @@ class LaravelAdorableTest extends FeatureTestCase
 
         $image = $adorable->get(200, '767c9d26-5484-438d-a614-858f0b420fcc');
         $avatar = file_get_contents(__DIR__.'/../../stubs/avatar1.png');
+        $base64 = 'data:image/png;base64,'.base64_encode($avatar);
 
-        $this->assertEquals($avatar, $image);
+        $this->assertEquals($base64, $image);
         $this->assertTrue($cache->has('adorable.042d0af447682411dd93493f94cf1b7a9aa1ceb26a8c7b75d2a88ba07dbcc89d'));
-        $this->assertEquals($avatar, $cache->get('adorable.042d0af447682411dd93493f94cf1b7a9aa1ceb26a8c7b75d2a88ba07dbcc89d'));
+        $this->assertEquals($base64, $cache->get('adorable.042d0af447682411dd93493f94cf1b7a9aa1ceb26a8c7b75d2a88ba07dbcc89d'));
     }
 
     /**
@@ -33,9 +34,10 @@ class LaravelAdorableTest extends FeatureTestCase
 
         $image = $adorable->get(200, '767c9d26-5484-438d-a614-858f0b420fcc');
         $avatar = file_get_contents(__DIR__.'/../../stubs/avatar2.png');
+        $base64 = 'data:image/png;base64,'.base64_encode($avatar);
 
-        $this->assertEquals($avatar, $image);
+        $this->assertEquals($base64, $image);
         $this->assertTrue($cache->has('adorable.b6cb42a351d2b3786df2efefaca062d032a67d7e1da38260102e1bdd35965e97'));
-        $this->assertEquals($avatar, $cache->get('adorable.b6cb42a351d2b3786df2efefaca062d032a67d7e1da38260102e1bdd35965e97'));
+        $this->assertEquals($base64, $cache->get('adorable.b6cb42a351d2b3786df2efefaca062d032a67d7e1da38260102e1bdd35965e97'));
     }
 }
