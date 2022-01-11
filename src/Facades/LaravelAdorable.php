f286cdc6bc61a98d089a1d1c917c785a01b68ac3
Alexis Saettler <alexis.saettler@wolterskluwer.com>
2022-01-11 19:43:57 +0100

    fix: fix resize with parameter (#7)

--- src/Facades/LaravelAdorable.php
+++ src/Facades/LaravelAdorable.php
@@ -5,6 +5,8 @@ namespace LaravelAdorable\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static string get(string $size, string $uuid)
+ *
  * @see \LaravelAdorable\Service\LaravelAdorable
  */
 class LaravelAdorable extends Facade
